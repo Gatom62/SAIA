d4d6edcf4d9aaec81efbad2fc5878cc380ffc664
Gatom62 <20240046@ricaldone.edu.sv>
2024-09-30 12:14:52 -0600

    Añadi a mi rama el cambio de cierre de caja

---
+++ Modelo/DAO/DTOCierreCaja.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgroServicios.Modelo.DAO
+{
+    class DTOCierreCaja : dbContext
+    {
+    }
+}
---
+++ Vista/MenuPrincipal/VistaCerrarCaja.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace AgroServicios.Vista.MenuPrincipal
+{
+    partial class VistaCerrarCaja
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VistaCerrarCaja));
+            Bunifu.UI.WinForms.BunifuButton.BunifuButton2.BorderEdges borderEdges1 = new Bunifu.UI.WinForms.BunifuButton.BunifuButton2.BorderEdges();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCerrarCaja = new Bunifu.UI.WinForms.BunifuButton.BunifuButton2();
+            this.dgvCierre = new Bunifu.UI.WinForms.BunifuDataGridView();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCierre)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(147)))), ((int)(((byte)(233)))), ((int)(((byte)(252)))));
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnCerrarCaja, 3, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1371, 86);
+            this.tableLayoutPanel2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(301, 47);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cierre de caja";
+            // 
+            // btnCerrarCaja
+            // 
+            this.btnCerrarCaja.AllowAnimations = true;
+            this.btnCerrarCaja.AllowMouseEffects = true;
+            this.btnCerrarCaja.AllowToggling = false;
+            this.btnCerrarCaja.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnCerrarCaja.AnimationSpeed = 200;
+            this.btnCerrarCaja.AutoGenerateColors = false;
+            this.btnCerrarCaja.AutoRoundBorders = false;
+            this.btnCerrarCaja.AutoSizeLeftIcon = true;
+            this.btnCerrarCaja.AutoSizeRightIcon = true;
+            this.btnCerrarCaja.BackColor = System.Drawing.Color.Transparent;
+            this.btnCerrarCaja.BackColor1 = System.Drawing.Color.DodgerBlue;
+            this.btnCerrarCaja.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnCerrarCaja.BackgroundImage")));
+            this.btnCerrarCaja.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton2.BorderStyles.Solid;
+            this.btnCerrarCaja.ButtonText = "Cerrar caja";
+            this.btnCerrarCaja.ButtonTextMarginLeft = 0;
+            this.btnCerrarCaja.ColorContrastOnClick = 45;
+            this.btnCerrarCaja.ColorContrastOnHover = 45;
+            this.btnCerrarCaja.Cursor = System.Windows.Forms.Cursors.Default;
+            borderEdges1.BottomLeft = true;
+            borderEdges1.BottomRight = true;
+            borderEdges1.TopLeft = true;
+            borderEdges1.TopRight = true;
+            this.btnCerrarCaja.CustomizableEdges = borderEdges1;
+            this.btnCerrarCaja.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.btnCerrarCaja.DisabledBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
+            this.btnCerrarCaja.DisabledFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.btnCerrarCaja.DisabledForecolor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(160)))), ((int)(((byte)(168)))));
+            this.btnCerrarCaja.FocusState = Bunifu.UI.WinForms.BunifuButton.BunifuButton2.ButtonStates.Pressed;
+            this.btnCerrarCaja.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCerrarCaja.ForeColor = System.Drawing.Color.White;
+            this.btnCerrarCaja.IconLeftAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCerrarCaja.IconLeftCursor = System.Windows.Forms.Cursors.Default;
+            this.btnCerrarCaja.IconLeftPadding = new System.Windows.Forms.Padding(11, 3, 3, 3);
+            this.btnCerrarCaja.IconMarginLeft = 11;
+            this.btnCerrarCaja.IconPadding = 10;
+            this.btnCerrarCaja.IconRightAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCerrarCaja.IconRightCursor = System.Windows.Forms.Cursors.Default;
+            this.btnCerrarCaja.IconRightPadding = new System.Windows.Forms.Padding(3, 3, 7, 3);
+            this.btnCerrarCaja.IconSize = 25;
+            this.btnCerrarCaja.IdleBorderColor = System.Drawing.Color.DodgerBlue;
+            this.btnCerrarCaja.IdleBorderRadius = 20;
+            this.btnCerrarCaja.IdleBorderThickness = 1;
+            this.btnCerrarCaja.IdleFillColor = System.Drawing.Color.DodgerBlue;
+            this.btnCerrarCaja.IdleIconLeftImage = null;
+            this.btnCerrarCaja.IdleIconRightImage = null;
+            this.btnCerrarCaja.IndicateFocus = false;
+            this.btnCerrarCaja.Location = new System.Drawing.Point(1046, 11);
+            this.btnCerrarCaja.Name = "btnCerrarCaja";
+            this.btnCerrarCaja.OnDisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
+            this.btnCerrarCaja.OnDisabledState.BorderRadius = 20;
+            this.btnCerrarCaja.OnDisabledState.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton2.BorderStyles.Solid;
+            this.btnCerrarCaja.OnDisabledState.BorderThickness = 1;
+            this.btnCerrarCaja.OnDisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.btnCerrarCaja.OnDisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(160)))), ((int)(((byte)(168)))));
+            this.btnCerrarCaja.OnDisabledState.IconLeftImage = null;
+            this.btnCerrarCaja.OnDisabledState.IconRightImage = null;
+            this.btnCerrarCaja.onHoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(181)))), ((int)(((byte)(255)))));
+            this.btnCerrarCaja.onHoverState.BorderRadius = 20;
+            this.btnCerrarCaja.onHoverState.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton2.BorderStyles.Solid;
+            this.btnCerrarCaja.onHoverState.BorderThickness = 1;
+            this.btnCerrarCaja.onHoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(181)))), ((int)(((byte)(255)))));
+            this.btnCerrarCaja.onHoverState.ForeColor = System.Drawing.Color.White;
+            this.btnCerrarCaja.onHoverState.IconLeftImage = null;
+            this.btnCerrarCaja.onHoverState.IconRightImage = null;
+            this.btnCerrarCaja.OnIdleState.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.btnCerrarCaja.OnIdleState.BorderRadius = 20;
+            this.btnCerrarCaja.OnIdleState.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton2.BorderStyles.Solid;
+            this.btnCerrarCaja.OnIdleState.BorderThickness = 1;
+            this.btnCerrarCaja.OnIdleState.FillColor = System.Drawing.Color.DodgerBlue;
+            this.btnCerrarCaja.OnIdleState.ForeColor = System.Drawing.Color.White;
+            this.btnCerrarCaja.OnIdleState.IconLeftImage = null;
+            this.btnCerrarCaja.OnIdleState.IconRightImage = null;
+            this.btnCerrarCaja.OnPressedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(96)))), ((int)(((byte)(144)))));
+            this.btnCerrarCaja.OnPressedState.BorderRadius = 20;
+            this.btnCerrarCaja.OnPressedState.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton2.BorderStyles.Solid;
+            this.btnCerrarCaja.OnPressedState.BorderThickness = 1;
+            this.btnCerrarCaja.OnPressedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(96)))), ((int)(((byte)(144)))));
+            this.btnCerrarCaja.OnPressedState.ForeColor = System.Drawing.Color.White;
+            this.btnCerrarCaja.OnPressedState.IconLeftImage = null;
+            this.btnCerrarCaja.OnPressedState.IconRightImage = null;
+            this.btnCerrarCaja.Size = new System.Drawing.Size(305, 63);
+            this.btnCerrarCaja.TabIndex = 1;
+            this.btnCerrarCaja.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnCerrarCaja.TextAlignment = System.Windows.Forms.HorizontalAlignment.Center;
+            this.btnCerrarCaja.TextMarginLeft = 0;
+            this.btnCerrarCaja.TextPadding = new System.Windows.Forms.Padding(0);
+            this.btnCerrarCaja.UseDefaultRadiusAndThickness = true;
+            // 
+            // dgvCierre
+            // 
+            this.dgvCierre.AllowCustomTheming = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(251)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black;
+            this.dgvCierre.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvCierre.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvCierre.BackgroundColor = System.Drawing.Color.White;
+            this.dgvCierre.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvCierre.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dgvCierre.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.DodgerBlue;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI Semibold", 11.75F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(115)))), ((int)(((byte)(204)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvCierre.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvCierre.ColumnHeadersHeight = 40;
+            this.dgvCierre.CurrentTheme.AlternatingRowsStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(251)))), ((int)(((byte)(255)))));
+            this.dgvCierre.CurrentTheme.AlternatingRowsStyle.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.dgvCierre.CurrentTheme.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Black;
+            this.dgvCierre.CurrentTheme.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(232)))), ((int)(((byte)(255)))));
+            this.dgvCierre.CurrentTheme.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.dgvCierre.CurrentTheme.BackColor = System.Drawing.Color.White;
+            this.dgvCierre.CurrentTheme.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(238)))), ((int)(((byte)(255)))));
+            this.dgvCierre.CurrentTheme.HeaderStyle.BackColor = System.Drawing.Color.DodgerBlue;
+            this.dgvCierre.CurrentTheme.HeaderStyle.Font = new System.Drawing.Font("Segoe UI Semibold", 11.75F, System.Drawing.FontStyle.Bold);
+            this.dgvCierre.CurrentTheme.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.dgvCierre.CurrentTheme.HeaderStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(115)))), ((int)(((byte)(204)))));
+            this.dgvCierre.CurrentTheme.HeaderStyle.SelectionForeColor = System.Drawing.Color.White;
+            this.dgvCierre.CurrentTheme.Name = null;
+            this.dgvCierre.CurrentTheme.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.dgvCierre.CurrentTheme.RowsStyle.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.dgvCierre.CurrentTheme.RowsStyle.ForeColor = System.Drawing.Color.Black;
+            this.dgvCierre.CurrentTheme.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(232)))), ((int)(((byte)(255)))));
+            this.dgvCierre.CurrentTheme.RowsStyle.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(232)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvCierre.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvCierre.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvCierre.EnableHeadersVisualStyles = false;
+            this.dgvCierre.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(238)))), ((int)(((byte)(255)))));
+            this.dgvCierre.HeaderBackColor = System.Drawing.Color.DodgerBlue;
+            this.dgvCierre.HeaderBgColor = System.Drawing.Color.Empty;
+            this.dgvCierre.HeaderForeColor = System.Drawing.Color.White;
+            this.dgvCierre.Location = new System.Drawing.Point(0, 86);
+            this.dgvCierre.Name = "dgvCierre";
+            this.dgvCierre.RowHeadersVisible = false;
+            this.dgvCierre.RowHeadersWidth = 51;
+            this.dgvCierre.RowTemplate.Height = 40;
+            this.dgvCierre.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvCierre.Size = new System.Drawing.Size(1371, 552);
+            this.dgvCierre.TabIndex = 6;
+            this.dgvCierre.Theme = Bunifu.UI.WinForms.BunifuDataGridView.PresetThemes.Light;
+            // 
+            // VistaCerrarCaja
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1371, 638);
+            this.Controls.Add(this.dgvCierre);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Name = "VistaCerrarCaja";
+            this.Text = "VistaCerrarCaja";
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCierre)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label1;
+        public Bunifu.UI.WinForms.BunifuButton.BunifuButton2 btnCerrarCaja;
+        public Bunifu.UI.WinForms.BunifuDataGridView dgvCierre;
+    }
+}
